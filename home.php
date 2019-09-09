ab4ade4851612220366286b320a2582d3885d7a1
randimatharu <randimatharuni@gmail.com>
2019-09-09 12:33:07 +0530

    new home

--- home.php
+++ home.php
@@ -1,187 +1,88 @@
 <!DOCTYPE html>
 <html>
-<head>
-<style>
-* {
-  box-sizing: border-box;
-}
+  <head>
+    <style>
+      * {
+        box-sizing: border-box;
+      }
 
-body {
-  font-family: Arial;
-  padding: 10px;
-  background: #f1f1f1;
-}
+      body {
+        font-family: Arial;
+        padding: 10px;
+        background: #f1f1f1;
+      }
 
-/* Header/Blog Title */
-.header {
-  padding: 30px;
-  text-align: center;
-  background: white;
-}
+      /* Header/Blog Title */
+      .header {
+        padding: 30px;
+        text-align: center;
+        background: white;
+      }
 
-.header h1 {
-  font-size: 50px;
-}
+      /* Header/Heading */
+      .header h1 {
+        font-size: 50px;
+      }
 
-/* Style the top navigation bar */
-.topnav {
-  overflow: hidden;
-  background-color: #333;
-}
+      /* Style the top navigation bar */
+      .topnav {
+        overflow: hidden;
+        background-color: #333;
+      }
 
-/* Style the topnav links */
-.topnav a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
+      /* Style the topnav links */
+      .topnav a {
+        float: left;
+        display: block;
+        color: #f2f2f2;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        float:right;
+      }
 
-/* Change color on hover */
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
+      /* Change color on hover */
+      .topnav a:hover {
+        background-color: #ddd;
+        color: black;
+      }
 
-/* Create two unequal columns that floats next to each other */
-/* Left column */
-.leftcolumn {   
-  float: left;
-  width: 75%;
-}
+      /* Clear floats after the columns */
+      .row:after {
+        content: "";
+        display: table;
+        clear: both;
+      }
 
-/* Right column */
-.rightcolumn {
-  float: left;
-  width: 25%;
-  background-color: #f1f1f1;
-  padding-left: 20px;
-}
+      /* Footer */
+      .footer {
+        padding: 30px;
+        text-align: center;
+        background: #ddd;
+        
+      }
+    </style>
+  </head>
+  <body>
 
-/* Create three equal columns that floats next to each other */
-.column {
-  float: left;
-  width: 33.33%;
-  padding: 15px;
-}
-
-/* Fake image */
-.fakeimg {
-  background-color: #aaa;
-  width: 100%;
-  padding: 20px;
-}
-
-/* Add a card effect for articles */
-.card {
-  background-color: white;
-  padding: 20px;
-  margin-top: 20px;
-}
-
-/* Add a marquee_card effect for articles */
-.marquee_card {
-  background-color: white;
-  padding: 30px;
-}
-
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Footer */
-.footer {
-  padding: 30px;
-  text-align: center;
-  background: #ddd;
-  margin-top: 20px;
-}
-
-.sticky {
-  position: fixed;
-  top: 0;
-  width: 100%;
-}
-
-.sticky + .content {
-  padding-top: 102px;
-}
-
-/* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other */
-@media screen and (max-width: 800px) {
-  .leftcolumn, .rightcolumn {   
-    width: 100%;
-    padding: 0;
-  }
-}
-
-/* Responsive layout - when the screen is less than 400px wide, make the navigation links stack on top of each other instead of next to each other */
-@media screen and (max-width: 400px) {
-  .topnav a {
-    float: none;
-    width: 100%;
-  }
-}
-</style>
-</head>
-<body>
-
-<div class="header" id="myHeader">
-  <img src="Images/logo.png">
-</div>
-
-<div class="topnav">
-  <a href="#">Link</a>
-  <a href="#">Link</a>
-  <a href="#">Link</a>
-  <a href="#" style="float:right">Link</a>
-</div>
-
-<div class="marquee_card">
-      <h3><marquee scrollamount="6">Welcome to SLIC internal site</marquee></h3>
+    <div class="header" id="myHeader">
+      <img src="Images/logo.png">
     </div>
 
-<div class="row">
-  <div class="column">
-    <h2>Registration</h2>
-    <img src="Images/user.png">
-  </div>
-  
-  <div class="column">
-  <h2>Registration</h2>
-    <img src="Images/view.jpg" width="90%">
+    <div class="topnav">
+      <a href="help.php">Help</a>
+      <a href="inventors.php">Inventors</a>
+      <a href="#">Home</a>
     </div>
-  
-  <div class="column">
-  <h2>Registration</h2>
-    <img src="Images/user.png">
+
+    <div class="row">
+        <img src="Images/inno.jpg" width="100%" heigth="50%">
     </div>
-</div>
 
-<div class="footer">
-  <p>© 2017, All rights reserved by Sri Lanka Inventors Commission</p>
-  <p>46-48, Cotta Road, Colombo 08</p>
-</div>
+    <div class="footer">
+      <p>© 2017, All rights reserved by Sri Lanka Inventors Commission</p>
+      <p>46-48, Cotta Road, Colombo 08</p>
+    </div>
 
-<script>
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-</script>
-
-</body>
+  </body>
 </html>
